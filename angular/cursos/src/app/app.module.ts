f22cb7fbf9f4322452b2cdd5b1c659deba92c7c6
Victor Tena Palomares <victor.tena@viewnext.com>
2021-09-06 14:40:35 +0200

    commit > 16. Paginación añadida. Todo ok trabajando con zuul

--- angular/cursos/src/app/app.module.ts
+++ angular/cursos/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ExamenesComponent } from './components/examenes/examenes.component';
 import { LayoutModule } from './layout/layout.module';
 import { AlumnosFormComponent } from './components/alumnos/alumnos-form.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
@@ -22,8 +24,9 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
+    MatPaginatorModule, // modulo proporcionado por la librería material.angular.io
     FormsModule, // Modulo necesario para mapeo de campos desde el formulario a la clase typescript donde se encuentran los atributos que se van a poblar con esta información
-    HttpClientModule // cliente http para consumo de recursos del backend
+    HttpClientModule, BrowserAnimationsModule // cliente http para consumo de recursos del backend
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/cursos/src/app/components/alumnos/alumnos.component.ts
+++ angular/cursos/src/app/components/alumnos/alumnos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { Alumno } from 'src/app/models/alumno';
 import { AlumnoService } from 'src/app/services/alumno.service';
 import Swal from 'sweetalert2'
@@ -11,6 +12,12 @@ export class AlumnosComponent implements OnInit {
 
   titulo = 'Listado de Alumnos';
   alumnos: Alumno[];
+  totalRegistros: number = 0;
+  totalPorPagina: number = 4;
+  paginaActual: number = 0;
+  pageSizeOptions: number[] = [3, 5, 10, 25, 100];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator; // para inyectar un componente hijo que está dentro del padreen el padre
 
 
   //inyección de la capa service en el componente
@@ -18,9 +25,24 @@ export class AlumnosComponent implements OnInit {
 
   // todas las comunicaciones que se hacen con el backend se tienen que implementar en el ngOnInit
   ngOnInit(): void {
-    this.service.listar().subscribe(alumnos => { //Sacamos los alumnos que nos han venido desde el backend y se los pasamos al atributo de esta clase
-      this.alumnos = alumnos; // este código es el observador que consume al observable, para poder hacerlo nos tenemos que subscribir...
-    });
+    this.calcularRangos();
+  }
+
+  public paginar(event: PageEvent): void {
+    this.paginaActual = event.pageIndex;
+    this.totalPorPagina = event.pageSize;
+    this.calcularRangos();
+
+
+  }
+
+  private calcularRangos() {
+    this.service.listarPaginas(this.paginaActual.toString(),
+      this.totalPorPagina.toString()).subscribe(p => { //Sacamos los alumnos que nos han venido desde el backend y se los pasamos al atributo de esta clase. En la opción paginable convertimos al vuelo el number a string dentro del paso de parámetros
+        this.alumnos = p.content as Alumno[]; // este código es el observador que consume al observable, para poder hacerlo nos tenemos que subscribir...
+        this.totalRegistros = p.totalElements as number;
+        this.paginator._intl.itemsPerPageLabel="Registros por página"
+      });
   }
 
   public eliminar(alumno: Alumno): void {
@@ -35,13 +57,14 @@ export class AlumnosComponent implements OnInit {
     }).then((result) => {
       if (result.isConfirmed) {
         this.service.eliminar(alumno.id).subscribe(() => {
-          this.alumnos = this.alumnos.filter(alumnoQueSeHaQuedado => {
-            alumnoQueSeHaQuedado !== alumno;// El filtrado conservará a todos los alumnos que están en la lista que son diferntes al alumno que se recibe por parámetro
-            Swal.fire('Borrado:',`Alumno ${alumno.nombre} eliminado con Éxtito`,'question');
-          });
+          this.calcularRangos();
+          //this.alumnos = this.alumnos.filter(alumnoQueSeHaQuedado => {
+          //alumnoQueSeHaQuedado !== alumno;// El filtrado conservará a todos los alumnos que están en la lista que son diferntes al alumno que se recibe por parámetro
+          Swal.fire('Borrado:', `Alumno ${alumno.nombre} eliminado con Éxtito`, 'question');
+
         });
       }
-    });    
+    });
   }
 
 }
